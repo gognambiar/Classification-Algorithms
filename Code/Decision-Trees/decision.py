7a52e4cca7d8db3e23980195be08b98653a00602
Moonis Javed <monis.javed@gmail.com>
2017-11-22 16:33:13 -0500

    Added code for Decision Trees

---
+++ Code/Decision-Trees/decision.py
@@ -0,0 +1,193 @@
+import numpy as np
+from pandas import DataFrame
+# from sklearn.metrics.pairwise import euclidean_distances
+import os
+import sys
+import getopt
+import copy
+import argparse
+import simplejson
+
+
+def loadData(filePath):
+    # function to load data from file
+
+    # if file doesnt exist then return None
+    if not os.path.exists(filePath):
+        return None
+
+    # load data from the file
+    df = DataFrame.from_csv(filePath, sep='\s+', header=None, index_col=False)
+    # print df
+
+    obj_cols = df.select_dtypes(include=['object']).columns.values.tolist()
+
+    for col in obj_cols:
+        df[col] = df[col].astype('category')
+
+    obj_cols = df.select_dtypes(['category']).columns
+
+    df[obj_cols] = df[obj_cols].apply(lambda x: x.cat.codes)
+
+    data = df.values
+
+
+    # extract labels and data
+    labels = data[:,-1]
+    data = data[:,:-1]
+
+    return (data,labels)
+
+
+class DecisionTree(object):
+
+    def __init__(self, maxDepth=1, minRows=1):
+        self.maxDepth = maxDepth
+        self.minRows = minRows
+        self.root = None
+
+    def fit(self, data, labels):
+        self.data = data
+        self.labels = labels
+        self.data = np.hstack((data,labels.reshape(-1,1)))
+        # print self.data.shape
+        self.root = self.createTree(self.data)
+
+        # print simplejson.dumps(self.root)
+
+    def getGini(self, data, classes):
+        gini = 1
+        for val in classes:
+            gini -= (np.count_nonzero(data[:,-1] == val) / float(data.shape[0])) ** 2
+
+        return gini
+
+
+    def getSplit(self, data, gini):
+        selIndex = None
+        selValue = None
+        gain = None
+        selLeft = None
+        selRight = None
+        selGiniLeft = None
+        selGiniRight = None
+
+
+
+        for index in xrange(data.shape[1] - 1):
+            values = set(data[:,index].reshape(1,-1).tolist()[0])
+            for value in values:
+                # print index,value,values
+                left = data[data[:, index] < value]
+                right = data[data[:, index] >= value]
+                
+                if left.shape[0] == 0 or right.shape[0] == 0:
+                    continue
+                giniLeft = self.getGini(left, set(left[:,-1].reshape(1,-1).tolist()[0]))
+                giniRight = self.getGini(right, set(right[:,-1].reshape(1,-1).tolist()[0]))
+
+                # print left.shape,giniLeft
+                # print right.shape, giniRight
+
+                infoGain = gini - (giniLeft*(left.shape[0]/float(data.shape[0]))) - (giniRight*(right.shape[0]/float(data.shape[0])))
+
+                # print infoGain,gini,giniLeft,left.shape[0],giniRight,right.shape[0]
+
+                if gain is None or infoGain > gain:
+                    selIndex = index
+                    selValue = value
+                    gain = infoGain
+                    selLeft = left
+                    selRight = right
+                    selGiniLeft = giniLeft
+                    selGiniRight = giniRight
+
+        return (selIndex, selValue, gain, selLeft, selRight)
+
+
+
+
+
+    def createTree(self, data, depth=0):
+        # print data.shape,depth,self.minRows,self.maxDepth
+        gini = self.getGini(data,set(data[:,-1].reshape(1,-1).tolist()[0]))
+        root = {'data':data.tolist(), 'gini':gini, 'dataCount': data.shape[0]}
+        if gini == 0 or data.shape[0] < self.minRows or depth >= self.maxDepth:
+            root['type'] = 'terminal'
+            # print data.shape
+            unique, counts = np.unique(data[:,-1], return_counts=True)
+            # print unique,counts
+            count = zip(unique, counts)
+            count = sorted(count, key = lambda x: x[1], reverse=True)
+            # print count
+            root['label'] = count[0][0]
+            # exit(0)
+            # root['label'] = 
+            # print '\t'*depth,data
+            # return root
+        else:
+            root['type'] = 'split'
+            index, value, gain, left, right = self.getSplit(data, gini)
+            root['index'] = index
+            root['value'] = value
+            root['left'] = self.createTree(left,depth+1)
+            root['right'] = self.createTree(right,depth+1)
+            root['gain'] = gain
+            del root['data']
+        return root
+        # if gini = 
+
+    def predictUtil(self, row):
+        temp = self.root
+        while True:
+            if temp['type'] == 'terminal':
+                return temp['label']
+            index, val = temp['index'],temp['value']
+            if row[index] < val:
+                temp = temp['left']
+            else:
+                temp = temp['right']
+
+
+    def predict(self,data):
+        labels = np.apply_along_axis(self.predictUtil, 1, data)
+        return labels
+        
+
+def main(argv):
+    # arguments
+    parser = argparse.ArgumentParser(description='Hierarchial Agglomerative Clustering')
+
+    # optional arguments
+    # parser.add_argument('-o', '--output', help='Output file to store PCA visualization')
+
+    # required arguments
+    requiredNamed = parser.add_argument_group('Required named arguments')
+    requiredNamed.add_argument('-i', '--input', help='Input data file name', required=True, type=str)
+    requiredNamed.add_argument('-d', '--maxDepth', help='Maximum Depth of Decision Tree', required=True, type=int)
+    requiredNamed.add_argument('-r', '--minRows', help='Minimum Rows required to split', required=True, type=int)
+    args = parser.parse_args()
+    
+    # parse arguments
+    inputFile = args.input
+    maxDepth = args.maxDepth
+    minRows = args.minRows
+
+
+    # load initial data
+    data,labels = loadData(inputFile)
+
+    # data = np.array([[2,3,1],[4,5,0],[6,7,1],[8,9,0],[10,11,1]])
+    # print data.shape
+    # print labels.shape
+
+    tree = DecisionTree(maxDepth,minRows)
+    tree.fit(data,labels)
+    predicted =  tree.predict(data)
+    print np.sum(predicted == labels)/ float(labels.shape[0])
+    exit(0)
+
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
